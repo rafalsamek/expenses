b81b2a9e79baa3d83d52cf82a003b7dda3f88588
Rafal Samek <rsamek@datto.com>
2024-06-26 02:04:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# expenses
+A web application for expenses management
---
+++ backend/src/test/java/com/smartvizz/expenses/ExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartvizz.expenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
