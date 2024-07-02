50dbc06819a0a15409193a69913e4579e5cabd33
Rafal Samek <rsamek@datto.com>
2024-07-02 10:16:25 +0200

    rebuild_backend.sh

---
+++ rebuild_backend.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Name of the Docker Compose service
+SERVICE_NAME="backend"
+
+echo "Building the JAR file..."
+docker exec -it expenses_backend ./gradlew clean build -x test
+
+echo "Restarting service ${SERVICE_NAME}..."
+docker-compose restart $SERVICE_NAME
+
+echo "The application has been rebuilt and restarted."
