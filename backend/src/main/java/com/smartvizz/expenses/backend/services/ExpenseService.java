0dc2bfc425f13a2c3f055ee1bfcaba1b3b983d65
Rafal Samek <rsamek@datto.com>
2024-07-04 00:44:08 +0200

    ExpenseController with ExpenseService injection

---
+++ backend/src/main/java/com/smartvizz/expenses/backend/services/ExpenseService.java
@@ -0,0 +1,7 @@
+package com.smartvizz.expenses.backend.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExpenseService {
+}
---
+++ backend/src/main/java/com/smartvizz/expenses/backend/web/controllers/ExpenseController.java
@@ -0,0 +1,15 @@
+package com.smartvizz.expenses.backend.web.controllers;
+
+import com.smartvizz.expenses.backend.services.ExpenseService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/expenses")
+public class ExpenseController {
+    private final ExpenseService expenseService;
+
+    public ExpenseController(ExpenseService expenseService) {
+        this.expenseService = expenseService;
+    }
+}
