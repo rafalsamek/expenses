fc39495fa5a31874158ce3a76d34138a75613783
Rafal Samek <rsamek@datto.com>
2024-07-19 16:36:14 +0200

    frontend: left-menu and documents components

--- frontend/src/app/container/container.component.ts
+++ frontend/src/app/container/container.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {LeftMenuComponent} from "./left-menu/left-menu.component";
+import {DocumentsComponent} from "./documents/documents.component";
 
 @Component({
   selector: 'app-container',
   standalone: true,
-  imports: [],
+  imports: [
+    LeftMenuComponent,
+    DocumentsComponent
+  ],
   templateUrl: './container.component.html',
   styleUrl: './container.component.css'
 })
