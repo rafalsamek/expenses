ebb6fd5243e79cb32709f69b8bf92582896b5dd3
Rafal Samek <rsamek@datto.com>
2024-07-23 02:19:26 +0200

    backend: @CrossOrigin http://localhost:4200 added

--- backend/src/main/java/com/smartvizz/expenses/backend/web/controllers/ExpenseController.java
+++ backend/src/main/java/com/smartvizz/expenses/backend/web/controllers/ExpenseController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/expenses")
-@CrossOrigin(origins = "http://localhost:8888")
+@CrossOrigin(origins = {"http://localhost:8888", "http://localhost:4200"})
 public class ExpenseController {
     private final ExpenseService expenseService;
 
