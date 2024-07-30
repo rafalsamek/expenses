8c016e885eb930a10d3bb13abc2c6c34c26cd972
Rafal Samek <rsamek@datto.com>
2024-07-30 06:02:07 +0200

    backend: db migration updated - amount multiplied by 100 as they are in cents

--- backend/src/main/resources/dev/db/migration/V2__Insert_into_expenses_table.sql
+++ backend/src/main/resources/dev/db/migration/V2__Insert_into_expenses_table.sql
@@ -1,3 +1,3 @@
-INSERT INTO expenses (title, description, amount, currency) VALUES ('śniadanie', 'zakupy na śniadanie', 40, 'PLN');
-INSERT INTO expenses (title, description, amount, currency) VALUES ('obiad', 'zakupy na obiad', 20, 'USD');
-INSERT INTO expenses (title, description, amount) VALUES ('kolacja', 'zakupy na kolację', 30);
+INSERT INTO expenses (title, description, amount, currency) VALUES ('śniadanie', 'zakupy na śniadanie', 4000, 'PLN');
+INSERT INTO expenses (title, description, amount, currency) VALUES ('obiad', 'zakupy na obiad', 2000, 'USD');
+INSERT INTO expenses (title, description, amount, currency) VALUES ('kolacja', 'zakupy na kolację', 3000, NULL);
