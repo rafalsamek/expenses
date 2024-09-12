f53f066033003ab7b1e89c052bfa5923326729ee
Rafal Samek <rsamek@datto.com>
2024-09-12 05:06:06 +0200

    [EXPENSES-56] User should see only his own resources - in progress... - db migration - not needed migration removed

--- backend/src/main/resources/common/db/migration/U17__Drop_user_id_from_expenses_categories_table.sql
+++
@@ -1,7 +0,0 @@
-USE expenses;
-
-ALTER TABLE expenses_categories
-    DROP FOREIGN KEY fk_user_expense_category;
-
-ALTER TABLE expenses_categories
-    DROP COLUMN user_id;
--- backend/src/main/resources/common/db/migration/V17__Add_user_id_to_expenses_categories_table.sql
+++
@@ -1,5 +0,0 @@
-USE expenses;
-
-ALTER TABLE expenses_categories
-ADD COLUMN user_id BIGINT UNSIGNED NOT NULL DEFAULT 1 AFTER category_id,
-ADD CONSTRAINT fk_user_expense_category FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
