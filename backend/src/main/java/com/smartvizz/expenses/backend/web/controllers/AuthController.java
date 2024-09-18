e856a9e6f75addfd9114e945bdf07c96deb2013b
Rafal Samek <rsamek@datto.com>
2024-09-18 02:19:06 +0200

    prod: ports changed to production ones, .env.prod.dist added

--- backend/src/main/java/com/smartvizz/expenses/backend/web/controllers/AuthController.java
+++ backend/src/main/java/com/smartvizz/expenses/backend/web/controllers/AuthController.java
@@ -24,7 +24,7 @@ import java.util.Date;
 @CrossOrigin(origins = {
         "http://localhost:8888",
         "http://localhost:4200",
-        "http://162.55.215.13:8888",
+        "http://162.55.215.13:8898",
         "http://162.55.215.13:4200"
 })
 public class AuthController {
