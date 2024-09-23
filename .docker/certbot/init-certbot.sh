9c9967e774b41e225bed4457b26697e646f8eca7
Rafal Samek <rsamek@datto.com>
2024-09-23 23:41:39 +0200

    prod: init-certbot.sh updated

--- .docker/certbot/init-certbot.sh
+++ .docker/certbot/init-certbot.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
-certbot certonly --webroot --webroot-path=/var/www/certbot \
-  --email rafal.samek@gmail.com --agree-tos --no-eff-email \
-  -d expenses.smartvizz.com
+
+# Generate the certificate if it doesn't exist
+if [ ! -f "/etc/letsencrypt/live/expenses.smartvizz.com/fullchain.pem" ]; then
+  certbot certonly --webroot --webroot-path=/var/www/certbot \
+    --email rafal.samek@gmail.com --agree-tos --no-eff-email \
+    -d expenses.smartvizz.com
+fi
+
+# Start a loop to renew the certificate every 12 hours
+while :; do
+  certbot renew
+  sleep 12h
+done
